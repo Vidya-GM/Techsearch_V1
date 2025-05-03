4c6e14a4120f0738614a93635ced89454955a0aa
Vidya <vidyag10@gmail.com>
2025-05-03 19:42:30 +0200

    added and basic urls, templates and views

--- applications/projects/urls.py
+++ applications/projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from applications.projects import views
 
 urlpatterns = [
-    path('', views.projects, name="projects"),
+    path("", views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
 
 ]
--- applications/projects/views.py
+++ applications/projects/views.py
@@ -4,11 +4,48 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+#---------Only a text as output with HttpResponse ------
+# def projects(request):
+#     return HttpResponse('Here are our products')
+
+#-------- render a template with text -------
+# def projects(request):
+#    return render(request, "projects/projects.html")
+
+#------ A variable with template ---------
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
 def projects(request):
-    #return HttpResponse('Here are our products')
-    return render(request, "projects/projects.html")
+    msg = "Hello, you are on projects page"
+    num = 10
+    context = {"message": msg, "page": num, "projects": projectsList}
+    # return render(request, "projects/projects.html", {"message": msg})
+    return render(request, "projects/projects.html", context)
+
+# -------- single project --------
 
 
 def project(request, pk):
-    #return HttpResponse('Single projects' + str(pk))
-    return render(request, "projects/single-project.html")
+    projectObj = None
+    for i in projectsList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, "projects/single-project.html", {"projectid": projectObj})
